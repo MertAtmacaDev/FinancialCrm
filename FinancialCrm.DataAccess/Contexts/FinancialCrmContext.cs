d84369a13d62a133971f91ddb19cce9ce5abda86
Mert Atmaca <mertatmaca21@gmail.com>
2026-01-29 18:31:28 +0300

    Database created with EF core migrations

---
+++ FinancialCrm.DataAccess/Contexts/FinancialCrmContext.cs
@@ -0,0 +1,25 @@
+﻿using FinancialCrm.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialCrm.DataAccess.Contexts
+{
+    public class FinancialCrmContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=FinancialCrmDb;Username=postgres;Password=admin");
+        }
+
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<BankProcess> BankProcesses { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Spending> Spendings { get; set; }
+
+    }
+}
---
+++ FinancialCrm.DataAccess/Migrations/20260129152636_InitialCreate.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FinancialCrm.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banks",
+                columns: table => new
+                {
+                    BankId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BankTitle = table.Column<string>(type: "text", nullable: false),
+                    BankAccountNumber = table.Column<string>(type: "text", nullable: false),
+                    BankBalance = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banks", x => x.BankId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bills",
+                columns: table => new
+                {
+                    BillId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BillTitle = table.Column<string>(type: "text", nullable: false),
+                    BillAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    BillPeriod = table.Column<string>(type: "text", nullable: false),
+                    IsPaid = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bills", x => x.BillId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BankProcesses",
+                columns: table => new
+                {
+                    BankProcessId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ProcessTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ProcessType = table.Column<int>(type: "integer", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    BankId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankProcesses", x => x.BankProcessId);
+                    table.ForeignKey(
+                        name: "FK_BankProcesses_Banks_BankId",
+                        column: x => x.BankId,
+                        principalTable: "Banks",
+                        principalColumn: "BankId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Spendings",
+                columns: table => new
+                {
+                    SpendingId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SpendingTitle = table.Column<string>(type: "text", nullable: false),
+                    SpendingAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    SpendingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spendings", x => x.SpendingId);
+                    table.ForeignKey(
+                        name: "FK_Spendings_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BankProcesses_BankId",
+                table: "BankProcesses",
+                column: "BankId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Spendings_CategoryId",
+                table: "Spendings",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankProcesses");
+
+            migrationBuilder.DropTable(
+                name: "Bills");
+
+            migrationBuilder.DropTable(
+                name: "Spendings");
+
+            migrationBuilder.DropTable(
+                name: "Banks");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
